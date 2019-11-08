2bf1165acf4ad29fc75f7579e964fc6d8beca582
Chris McDowall <fogonwater@users.noreply.github.com>
2019-11-09 08:13:13 +1300

    Simplified bird viz code

--- 3_living_things/306_birds_feather/viz_bird_species.R
+++ 3_living_things/306_birds_feather/viz_bird_species.R
@@ -2,30 +2,33 @@ library(tidyr)
 library(ggplot2)
 library(dplyr)
 
+# specify a bird of interest
+bird_name = "Morepork"
+
+# load source data, including a csv of "blanks" to fill holes in land
 df1 <- read.csv("data/osnz_nz_1.csv")
 df2 <- read.csv("data/osnz_nz_2.csv")
 df3 <- read.csv("data/osnz_chathams.csv")
 df4 <- read.csv("data/osnz_blanks.csv")
 
-nzbo <- read.csv("data/bird_nz_status.csv") # load the nice NZBO table
-
-birds <- rbind(df1, df2, df4)
-
-bird_name = "Morepork"
-
-sel_bird <- inner_join(birds, nzbo) %>%
+# group all observations by grid coordinate,
+# then summarise on whether named bird was ever observered there
+sel_bird <- rbind(df1, df2, df4) %>%
   group_by(grid2) %>%
   summarize(ans = any(taxa.name == bird_name)) %>%
   separate(grid2, c("east", "north"), sep = ":", convert = TRUE)
 
+# scale easting/northing correctly
 sel_bird$east =  sel_bird$east  * 10000
 sel_bird$north = sel_bird$north * 10000
 
-write.csv(sel_bird, paste0("data/dist_", paste0(bird_name), ".csv"))
+# write to a csv named after the bird
+write.csv(sel_bird, paste0("data/species/", paste0(bird_name), ".csv"))
 
+# make a plot to see the output
 p <- ggplot(sel_bird, aes(east, north)) +
-  geom_point(aes(colour = ans), show.legend = TRUE) +
-  scale_color_manual(values=c("#ececec", "#BA4031"))+
+  geom_point(aes(colour = ans), show.legend = FALSE) +
+  scale_color_manual(values=c("#ECECEC", "#BA4031"))+
   theme_minimal() +
   coord_fixed()
 
